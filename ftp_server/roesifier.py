e04ff9b3f17df0c0461f6c42e4d095b1a489ac0b
roei sagiv <roeisagiv4@gmail.com>
2023-01-09 11:36:05 +0200

    modified ftp server

--- ftp_server/roesifier.py
+++ ftp_server/roesifier.py
@@ -11,10 +11,7 @@ with open("config.json") as jsonfile:
     configfile = json.load(jsonfile)
 
 # define redis on local host, on port 6379
-r = redis.StrictRedis(host='localhost', port=6379)
-
-# define the IP of the HAProxy"
-url = configfile["HaProxyUrl"]
+Redis_connection = redis.StrictRedis(host='localhost', port=6379)
 
 # create a new handler and connect the logger to logs.txt file
 logger = logging.getLogger(configfile["LoggerName"])
@@ -53,29 +50,29 @@ class OnMyWatch:
 
 class Handler(FileSystemEventHandler):
     @staticmethod
-    def on_any_event(event):
+    def on_closed(event):
         if event.is_directory:
             return None
         # action's when Event(FIle) is created:
-        elif event.event_type == 'created':
+        elif event.event_type == 'closed':
             # create variable with the name of the file
             file_name = event.src_path.replace(configfile["FtpTransferFiles"], '')
             logger.info(f'success - uploaded file to FTP server: {file_name}')
             # split name by basename and extension
             split_name = file_name.split("_")
             # if half of file alradey in redis, sends both to HAProxy
-            if r.exists(split_name[0]) == True:
+            if Redis_connection.exists(split_name[0]) == True:
                 print("-------------------------------")
                 first_half = event.src_path
-                second_half = (r.get(split_name[0])).decode()
+                second_half = (Redis_connection.get(split_name[0])).decode()
                 files = []
                 files.append(('files', open(first_half, 'rb')))
                 files.append(('files', open(second_half, 'rb')))
-                resp = requests.post(url=url, files=files)
+                resp = requests.post(url=configfile["HaProxyUrl"], files=files)
                 print("-------------------------------")
             # define first half file as Key in redis and the full path as value
             else:
-                r.set(split_name[0], event.src_path, ex=60)
+                Redis_connection.set(split_name[0], event.src_path, ex=60)
 
 
 if __name__ == '__main__':
