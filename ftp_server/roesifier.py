1857c315d85e7e34b619d884e4df6a2a662cb29b
roei sagiv <roeisagiv4@gmail.com>
2023-01-09 17:45:55 +0200

    updated roesifier func

--- ftp_server/roesifier.py
+++ ftp_server/roesifier.py
@@ -5,6 +5,7 @@ import multiprocessing
 import requests
 import logging
 import json
+import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -13,6 +14,9 @@ with open("/home/roeihafifot/config.json") as jsonfile:
 
 # define redis on local host, on port 6379
 Redis_connection = redis.StrictRedis(host='localhost', port=6379)
+
+# define FTP path to scan all files before watchdog client
+dir_list = os.listdir(configfile["FtpTransferFiles"])
 
 # create a new handler and connect the logger to logs.txt file
 logger = logging.getLogger(configfile["LoggerName"])
@@ -26,6 +30,24 @@ handler = logging.FileHandler(configfile["LogFile"])
 logger.addHandler(handler)
 
 
+def create_and_send(file):
+    # split name by basename and extension
+    split_name = file.split("_")
+    # if half of file alradey in redis, sends both to HAProxy
+    if Redis_connection.exists(split_name[0]) == True:
+        print("-------------------------------")
+        first_half = f'/ftphome/tranfer_files/{file}'
+        second_half = (Redis_connection.get(split_name[0])).decode()
+        files = []
+        files.append(('files', open(first_half, 'rb')))
+        files.append(('files', open(second_half, 'rb')))
+        resp = requests.post(url=configfile["HaProxyUrl"], files=files)
+        print("-------------------------------")
+    # define first half file as Key in redis and the full path as value
+    else:
+        Redis_connection.set(split_name[0], f'/ftphome/tranfer_files/{file}', ex=60)
+
+
 class OnMyWatch:
     # Set the directory on watch
     watchDirectory = configfile["FtpTransferFiles"]
@@ -34,6 +56,8 @@ class OnMyWatch:
         self.observer = Observer()
 
     def run(self):
+        for file in dir_list:
+            create_and_send(file)
         event_handler = Handler()
         self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
         self.observer.start()
@@ -59,21 +83,7 @@ class Handler(FileSystemEventHandler):
             # create variable with the name of the file
             file_name = event.src_path.replace(configfile["FtpTransferFiles"], '')
             logger.info(f'success - uploaded file to FTP server: {file_name}')
-            # split name by basename and extension
-            split_name = file_name.split("_")
-            # if half of file alradey in redis, sends both to HAProxy
-            if Redis_connection.exists(split_name[0]) == True:
-                print("-------------------------------")
-                first_half = event.src_path
-                second_half = (Redis_connection.get(split_name[0])).decode()
-                files = []
-                files.append(('files', open(first_half, 'rb')))
-                files.append(('files', open(second_half, 'rb')))
-                resp = requests.post(url=configfile["HaProxyUrl"], files=files)
-                print("-------------------------------")
-            # define first half file as Key in redis and the full path as value
-            else:
-                Redis_connection.set(split_name[0], event.src_path, ex=60)
+            create_and_send(file_name)
 
 
 if __name__ == '__main__':
