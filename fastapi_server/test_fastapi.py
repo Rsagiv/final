ab16db1fe0b06239ebefacd9c396c913ff2812f6
roei sagiv <roeisagiv4@gmail.com>
2023-01-22 19:24:35 +0200

    updated test for fastapi

--- fastapi_server/test_fastapi.py
+++ fastapi_server/test_fastapi.py
@@ -1,48 +1,27 @@
-import unittest
 import fastapifunc
-import os
+import unittest
 
 
-class TestEmployee(unittest.TestCase):
+
+class TestFastApi(unittest.TestCase):
 
     def setUp(self):
-        csvFilePath = r'MadaReports.csv'
-        jsonFilePath = r'jason.json'
-        jsonArray = main.csv_to_json(csvFilePath, jsonFilePath)
-        return jsonArray
+        return True
 
-    def test_json_correct(self):
-        jsonArray = self.setUp()
-        self.assertEqual(jsonArray['testuser']['IDNum'], '878746593')
-        self.assertEqual(jsonArray['testuser']['IDType'], '0')
-        self.assertEqual(jsonArray['testuser']['FirstName'], 'Garfield')
-        self.assertEqual(jsonArray['testuser']['LastName'], 'Sapseed')
-        self.assertEqual(jsonArray['testuser']['City'], 'Sinacaban')
-        self.assertEqual(jsonArray['testuser']['Street'], 'Mockingbird')
-        self.assertEqual(jsonArray['testuser']['BuildingNumber'], '36')
-        self.assertEqual(jsonArray['testuser']['Barcode'], '21ABBA1C-1292-5EB2-0BBA-7C4AB5DB1185')
-        self.assertEqual(jsonArray['testuser']['GetDate'], '28.04.2021')
-        self.assertEqual(jsonArray['testuser']['TakeDate'], '04.07.2021')
-        self.assertEqual(jsonArray['testuser']['ResultDate'], '23/03/2021')
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
 
-    def test_json_empty(self):
-        jsonArray = self.setUp()
-        self.assertEqual(jsonArray['empty_BuildingNumber_Barcode']['BuildingNumber'], '')
-        self.assertEqual(jsonArray['empty_BuildingNumber_Barcode']['Barcode'], '')
+    def test_redis_connection(self):
+        redis_connection = redis.StrictRedis(host='localhost', port=6379)
+        redis_check = roesifier.check_redis_connection(redis_connection)
+        self.assertTrue(redis_check)
 
-    def test_csvfile_exists_csv(self):
-        csvFilePath = r'MadaReports.csv'
-        self.assertTrue(os.path.isfile(csvFilePath))
-        self.assertTrue(csvFilePath.endswith('.csv'))
-
-    def test_jsonfile_exists_json(self):
-        jsonFilePath = r'jason.json'
-        self.assertTrue(os.path.isfile(jsonFilePath))
-        self.assertTrue(jsonFilePath.endswith('.json'))
-
-
-
-
+    def check_files_append(self):
+        file1 = open("example_a.txt", "w").write("this is")
+        file2 = open("example_b.txt", "w").write("a test")
+        roesifier.check_key_in_redis(file1)
+        roesifier.check_key_in_redis(file2)
 
 
 if __name__ == '__main__':
