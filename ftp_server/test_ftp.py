91aae17d20ffe19e726ad9860a8ef20cd8327fde
roei sagiv <roeisagiv4@gmail.com>
2023-01-22 19:00:57 +0200

    updated test_ftp

--- ftp_server/test_ftp.py
+++ ftp_server/test_ftp.py
@@ -17,6 +17,12 @@ class TestRoesifier(unittest.TestCase):
         redis_check = roesifier.check_redis_connection(redis_connection)
         self.assertTrue(redis_check)
 
+    def check_files_append(self):
+        file1 = open("example_a.txt", "w").write("this is")
+        file2 = open("example_b.txt", "w").write("a test")
+        roesifier.check_key_in_redis(file1)
+        roesifier.check_key_in_redis(file2)
+
 
 if __name__ == '__main__':
     unittest.main()
