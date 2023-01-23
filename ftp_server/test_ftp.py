dd245393263d838dbe3f92a12365c3614c64599d
roei sagiv <roeisagiv4@gmail.com>
2023-01-23 10:12:21 +0200

    changed format in test

--- ftp_server/test_ftp.py
+++ ftp_server/test_ftp.py
@@ -17,9 +17,9 @@ class TestRoesifier(unittest.TestCase):
         redis_check = roesifier.check_redis_connection(redis_connection)
         self.assertTrue(redis_check)
 
-    def check_files_append(self):
-        file1 = open("example_a.txt", "w").write("this is")
-        file2 = open("example_b.txt", "w").write("a test")
+    def test_check_files_append(self):
+        file1 = "example_a.txt"
+        file2 = "example_b.txt"
         print(roesifier.check_key_in_redis(file1))
         print(roesifier.check_key_in_redis(file2))
 
