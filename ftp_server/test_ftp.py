87cb553bbcd9291dbb879e32b2feb100e461bcf6
roei sagiv <roeisagiv4@gmail.com>
2023-01-23 10:54:17 +0200

    update tets

--- ftp_server/test_ftp.py
+++ ftp_server/test_ftp.py
@@ -1,6 +1,7 @@
 import roesifier
 import unittest
 import redis
+import os
 
 
 class TestRoesifier(unittest.TestCase):
@@ -10,16 +11,18 @@ class TestRoesifier(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('setupClass')
-
+        with open('/ftphome/tranfer_files/example_a.txt', "w") as file1:
+            os.system('sudo chmod +x /ftphome/tranfer_files/example_a.txt')
+            file1.write("this is")
+        with open('/ftphome/tranfer_files/example_b.txt', "w") as file2:
+            os.system('sudo chmod +x /ftphome/tranfer_files/example_b.txt')
+            file2.write("a test")
     def test_redis_connection(self):
         redis_connection = redis.StrictRedis(host='localhost', port=6379)
         redis_check = roesifier.check_redis_connection(redis_connection)
         self.assertTrue(redis_check)
 
     def test_check_files_append(self):
-        file1 = open("/ftphome/tranfer_files/example_a.txt", "w").write("this is")
-        file2 = open("/ftphome/tranfer_files/example_b.txt", "w").write("a test")
         file1name = "example_a.txt"
         file2name = "example_b.txt"
         print(roesifier.check_key_in_redis(file1name))
