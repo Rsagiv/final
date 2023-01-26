67112a071ba3868893feef65b4f6b270656681b7
roei sagiv <roeisagiv4@gmail.com>
2023-01-26 15:04:14 +0200

    fix ftptest

--- Tests/test_ftp.py
+++ Tests/test_ftp.py
@@ -4,29 +4,7 @@ import redis
 import os
 import warnings
 import json
-
-
-def check_json_connection(json_file_path):
-    try:
-        with open(json_file_path) as jsonfile:
-            json.load(jsonfile)
-        return True
-    except json.decoder.JSONDecodeError:
-        return False
-    except FileNotFoundError:
-        return "no_file"
-
-
-def import_config_file():
-    json_file_path = "/home/roeihafifot/config.json"
-    if check_json_connection(json_file_path):
-        with open("/home/roeihafifot/config.json") as jsonfile:
-            configfile = json.load(jsonfile)
-            return configfile
-    elif check_json_connection(json_file_path) == "no_file":
-        logger.info("ERROR In finding config file")
-    else:
-        logger.info("ERROR Connection to JSON file failed")
+import final.utils.mainutils as utils
 
 
 class TestRoesifier(unittest.TestCase):
@@ -36,7 +14,8 @@ class TestRoesifier(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        configfile = import_config_file()
+        configfile = utils.import_config_file("/home/roeihafifot/config.json")
+        logger = utils.create_logger(configfile["LoggerName"], configfile["LogFormatter"], configfile["LogFile"])
         redis_connection = redis.StrictRedis(host='localhost', port=6379)
         test_file1_contents = b"this is"
         file2_contents = b"a test"
@@ -49,8 +28,7 @@ class TestRoesifier(unittest.TestCase):
         return redis_connection
 
     def test_redis_connection(self):
-        redis_connection = self.setUpClass()
-        redis_check = roesifier.check_redis_connection(redis_connection)
+        redis_check = roesifier.redis_function().ping()
         self.assertTrue(redis_check)
 
     def test_redis_func(self):
